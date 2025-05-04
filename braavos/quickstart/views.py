e6a26276c9e77a7f86cb06453035bc7d0f0bbd4c
rapsealk <piono623@naver.com>
2025-05-04 13:14:22 +0900

    feat: Add simple prime numbers API

--- braavos/quickstart/views.py
+++ braavos/quickstart/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from braavos.quickstart.serializers import GroupSerializer, UserSerializer
 
@@ -22,3 +24,34 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by("name")
     serializer_class = GroupSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class PrimeNumberViewSet(viewsets.ViewSet):
+    """
+    API endpoint that allows prime numbers to be viewed or edited.
+    """
+
+    permission_classes = [permissions.AllowAny]
+
+    def list(self, request: Request) -> Response:
+        """
+        Return a list of prime numbers.
+        """
+        n = int(request.query_params.get("n", 0))
+        primes = self._get_primse(n)
+        return Response(primes)
+
+    def _get_primse(self, n: int) -> "list[int]":
+        """
+        Return a list of prime numbers.
+        """
+        result = []
+        for i in range(2, n):
+            is_prime = True
+            for j in range(2, i):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                result.append(i)
+        return result
